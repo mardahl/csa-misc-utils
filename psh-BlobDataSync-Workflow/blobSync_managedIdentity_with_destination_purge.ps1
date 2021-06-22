333c911e5f1da1d59997cdd942939a9074608c52
Michael Mardahl <iphase@iphase.dk>
2021-06-23 00:16:18 +0200

    Create blobSync_managedIdentity_with_destination_purge.ps1
    The original script was never really able to delete anything in the destination as there was no actual code to delete blobs. 
    So I created this version that will also purge destination of any files no longer in source.

---
+++ psh-BlobDataSync-Workflow/blobSync_managedIdentity_with_destination_purge.ps1
@@ -0,0 +1,146 @@
+Workflow blobSync {   
+    $PSDisableSerializationPreference = $true
+    $MaxReturn = 800;
+    [int64]$Total = 0;
+    [int64]$FilesDeleted = 0;
+    $Token = $null;
+    [int64]$FilesTransferred = 0;
+    [int64]$FilesTransferSuccess = 0;
+    [int64]$FilesTransferFail = 0;
+    $runflag = $TRUE;
+    [string]$containerName = 'myContainer'
+    $srcStorageAccount = "sourceStorageAccount";
+    $deststorageAccount = "destinationStorageAccount";
+
+    Connect-AzAccount -Identity
+
+    $TimeStart = Get-Date -format HH:mm:ss
+    "Destination Array Population...";
+    $DestBlobsHash = @{};
+    $DestBlobsHash = InlineScript
+    {
+        $DestBlobsHashTemp = @{}
+        $destContext = New-AzStorageContext -StorageAccountName $using:deststorageAccount
+           
+        Get-AzStorageBlob -Context $destContext -Container $using:containerName -ContinuationToken $Token  | 
+            Select-Object -Property Name, LastModified, ContinuationToken  | 
+            ForEach { $DestBlobsHashTemp[$_.Name] = $_.LastModified.UtcDateTime };
+        $DestBlobsHashTemp ; 
+    }
+
+    #COPY JOB
+
+    DO {
+        "Saving State...";    
+        Checkpoint-Workflow  #on each iteration persist our state  
+        "Source Array Population...";     
+        $SrcBlobs = InlineScript
+        {
+            $sourceContext = New-AzStorageContext -StorageAccountName $Using:srcStorageAccount
+            $SrcBlobs = Get-AzStorageBlob -Context $sourceContext -Container $Using:containerName -MaxCount $using:MaxReturn  -ContinuationToken $using:Token | 
+                Select-Object -Property Name, LastModified, ContinuationToken;
+
+            if ($SrcBlobs -ne $null) {
+                $cnt = $SrcBlobs.Count;
+                write-host "  ** Files Found: $cnt - Pulling $($using:MaxReturn) files at a time...";
+            }
+
+            $SrcBlobs;
+        }
+
+        $Total += $SrcBlobs.Count
+
+        if ($SrcBlobs.Length -le 0) { 
+            $runflag = $false;
+        }
+        $Token = $SrcBlobs[$SrcBlobs.Count - 1].ContinuationToken;
+
+        #Copy from source to destination
+        ForEach -parallel -ThrottleLimit 25  ($SrcBlob in $SrcBlobs) {
+            # search  in destination blobs for the source blob and unmodified, if found copy it
+            $CopyThisBlob = $false
+
+            if ($SrcBlob.Name -eq $null) {
+                "SrcBlob name is Null?";
+            }
+            elseif ($DestBlobsHash.count -eq 0) {
+                $CopyThisBlob = $true
+            }
+            elseif (!($DestBlobsHash.Contains($SrcBlob.Name))) {
+                $CopyThisBlob = $true
+            }
+            elseif ($SrcBlob.LastModified.UtcDateTime -gt $DestBlobsHash.Item($SrcBlob.Name)) {
+                $CopyThisBlob = $true
+            }
+
+            if ($CopyThisBlob) {
+                #Start copying the blobs to destination container
+                [string]$blobToCopy = $SrcBlob.Name
+                $UtcTime = Get-Date;
+                "  ++ Copying: $blobToCopy - " + $UtcTime.ToUniversalTime();
+                $workflow:FilesTransferred++;
+                try {
+                    inlinescript {
+                        $destContext = New-AzStorageContext -StorageAccountName $using:deststorageAccount
+                        $sourceContext = New-AzStorageContext -StorageAccountName $using:srcStorageAccount
+                        $c = Start-AzStorageBlobCopy -SrcContainer $using:containerName -SrcBlob $using:blobToCopy -DestContainer $using:containerName -DestBlob $using:blobToCopy -SrcContext $sourceContext -DestContext $destContext -Force
+                    }
+                    $workflow:FilesTransferSuccess++;
+                }
+                catch {
+                    Write-Error "$using:blobToCopy transfer failed"
+                    $workflow:FilesTransferFail++;
+                }   
+            }           
+        }
+    }
+    While (($Token -ne $Null) - ($runflag -eq $TRUE))
+
+    #PURGE JOB
+    "Saving State...";    
+    Checkpoint-Workflow  #on each iteration persist our state 
+    
+    $deleteDestBlobs = @();
+    $deleteDestBlobs = InlineScript
+    {
+        $tempArr = @()
+        #check to see if the file was removed from source and, if so, remove from destination
+        foreach ($blobName in $using:DestBlobsHash.keys) {
+            if(-not ($using:SrcBlobs.Name -Contains $blobName)){
+                $tempArr += $blobName
+            }
+        }
+        $tempArr;
+    }
+
+    #Remove files in destination that don't match source
+    ForEach -parallel -ThrottleLimit 25 ($blobToDelete in $deleteDestBlobs) {
+        "  -- blob: $blobToDelete was removed from primary store - deleting it from the backup"
+        $workflow:FilesDeleted++  
+        $destContext = New-AzStorageContext -StorageAccountName $using:deststorageAccount
+        Get-AzStorageBlob -Context $destContext -Container $using:containerName -Blob $blobToDelete | Remove-AzStorageBlob
+    }
+
+    #FINISH
+    
+    $TimeEnd = Get-Date -format HH:mm:ss
+    $TimeDiff = New-TimeSpan $TimeStart $TimeEnd
+    if ($TimeDiff.Seconds -lt 0) {
+        $Hrs = ($TimeDiff.Hours) + 23
+        $Mins = ($TimeDiff.Minutes) + 59
+        $Secs = ($TimeDiff.Seconds) + 59 
+    }
+    else {
+        $Hrs = $TimeDiff.Hours
+        $Mins = $TimeDiff.Minutes
+        $Secs = $TimeDiff.Seconds 
+    }
+    $Difference = '{0:00}:{1:00}:{2:00}' -f $Hrs, $Mins, $Secs
+   
+    "Total blobs in container $container : $Total"
+    "Total files transferred: $FilesTransferred"
+    "Total files Deleted: $FilesDeleted"
+    "Transfered successfully: $FilesTransferSuccess"
+    "Transfer failed: $FilesTransferFail"
+    "Elapsed time: $Difference `n"
+} 
